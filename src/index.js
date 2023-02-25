9df3329bf74aaf2f5186af9fd51c023773fd61e5
MashaMorits <moritcmasha@gmail.com>
2023-02-25 13:40:57 +0300

    иконка

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import './main.scss';
 import './index.html';
-// import "@fontsource/playfair-display";
-// import "@fontsource/playfair-display/cyrillic-700.css";
-// import '@fontsource/roboto/cyrillic-500.css'
-// import '@fontsource/roboto/cyrillic-300.css'
+
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-// import '@fontsource/roboto/700.css'
-// import '@fontsource/prata/cyrillic-400.css'
-// import '@fontsource/prata'
+
+
 
 
 
